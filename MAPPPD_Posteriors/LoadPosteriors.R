33bf8bb135e83aa2ea530a8343401457a29bd4dd
Heather Lynch <heather.lynch@stonybrook.edu>
2019-03-21 13:39:46 -0400

    Extract posteriors as matrix

--- MAPPPD_Posteriors/LoadPosteriors.R
+++ MAPPPD_Posteriors/LoadPosteriors.R
@@ -8,6 +8,14 @@
 #s3load(paste('ModelBuild/',version,'/MCMCzstate.rda', sep = ''), bucket = "penguinmap")
 #s3load(paste('ModelBuild/',version,'/SiteList.rda', sep = ''), bucket = "penguinmap")
 
+###Option 1: Create a Site x Year x Quantile matrix
+zstateList <- MCMCpstr(MCMCzstate, func = function(x) quantile(x, probs = c(0.025, .5, 0.975)))
+
+zstate <- zstateList[[1]]
+
+rownames(zstate) <- SiteList$site_id
+
+##Option #2: Pull out quantiles for particular named sites
 which(SiteList$site_id=="ADAM")
 MCMCsummary(MCMCzstate, 
             params = 'zstate\\[XYZ,', #substitute XYZ which the number from the previous call
@@ -15,11 +23,3 @@ MCMCsummary(MCMCzstate,
             round = 2, 
             func = function(x) quantile(x, probs = c(.05, .95)),
             func_name = c("5%", "95%"))
-
-#index<-as.character(which(SiteList$site_id=="ADAM"))
-#MCMCsummary(MCMCzstate, 
-#            params = noquote(paste("'zstate\\[",index,",'",sep="")), 
-#            ISB = FALSE, 
-#            round = 2, 
-#            func = function(x) quantile(x, probs = c(.05, .95)),
-#            func_name = c("5%", "95%"))
